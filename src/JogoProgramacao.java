12644086f9d6ebe52862d4f5d7731fffe3f6bad2
Afonso295 <152220916+Afonso295@users.noreply.github.com>
2024-12-15 16:53:13 +0000

    Ta aqui a solução dos vossos problemas
    # QUERO O MEU NOME NOS AGRADECIMENTOS
    
    Assinado: João

--- src/JogoProgramacao.java
+++ src/JogoProgramacao.java
@@ -80,7 +80,7 @@ public class JogoProgramacao {
 
     public void Explorar() {
         if (!gameOver) {
-            if (contagem_missao == missoes.size()) {
+            if (nave.getSubstancias() == 4) {
                 this.ultima_mensagem = "Você completou todas as missões!";
                 this.setMissaoAtual(null);
                 //JOptionPane.showMessageDialog(null, "Parabéns! Você completou todas as missões!", "Jogo Finalizado", JOptionPane.INFORMATION_MESSAGE);
@@ -120,7 +120,7 @@ public class JogoProgramacao {
         }
     }
 
-    // Método para criar as missões
+    // Metodo para criar as missões
     private List<Missao> CriarMissoes() {
         List<Missao> missoes = new ArrayList<>();
 
